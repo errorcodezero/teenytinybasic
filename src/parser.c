8f700a6f77d7a830ce668e8fdcd0b0d6384992b8
ErrorCode0 <me@errorcodezero.dev>
2026-01-01 09:49:02 +0530

    worked on part of statement parsing for parser

--- src/parser.c
+++ src/parser.c
@@ -9,18 +9,51 @@ Token *advance_parser(Parser *parser) {
   return &parser->tokens.list[parser->index++];
 }
 
+Token *peek_parser(Parser *parser) {
+  return &parser->tokens.list[parser->index];
+}
+
 void parse(Parser *parser) {
   while (!at_end_parser(parser)) {
     line(parser);
   }
 }
+
 void line(Parser *parser) {
-  Token *token = advance_parser(parser);
+  Token *token = peek_parser(parser);
   while (token->token_type != NEW_LINE || token->token_type != END_OF_FILE) {
     if (token->token_type == NUMBER) {
       // TODO: figure out numbered line for goto magic
+      wchar_t *number = token->data;
+      advance_parser(parser);
+      statement(parser);
     } else {
       statement(parser);
     }
+  }
+}
+
+void statement(Parser *parser) {
+  Token *token = peek_parser(parser);
+  if (token->token_type == PRINT) {
+    print_statement(parser);
+  } else if (token->token_type == GOTO) {
+    goto_statement(parser);
+  } else if (token->token_type == INPUT) {
+    input_statement(parser);
+  } else if (token->token_type == LET) {
+    let_statement(parser);
+  } else if (token->token_type == GOSUB) {
+    gosub_statement(parser);
+  } else if (token->token_type == RETURN) {
+    return_statement(parser);
+  } else if (token->token_type == CLEAR) {
+    clear_statement(parser);
+  } else if (token->token_type == LIST) {
+    list_statement(parser);
+  } else if (token->token_type == RUN) {
+    run_statement(parser);
+  } else if (token->token_type == END) {
+    end_statement(parser);
   }
 }
--- src/parser.h
+++ src/parser.h
@@ -14,17 +14,21 @@ void line(Parser *parser);
 void number(Parser *parser);
 void statement(Parser *parser);
 void print_statement(Parser *parser);
+void input_statement(Parser *parser);
 void if_statement(Parser *parser);
+void goto_statement(Parser *parser);
+void let_statement(Parser *parser);
 void relop(Parser *parser);
-void gosub(Parser *parser);
+void gosub_statement(Parser *parser);
 void return_statement(Parser *parser);
-void clear(Parser *parser);
-void list(Parser *parser);
-void run(Parser *parser);
-void end(Parser *parser);
+void clear_statement(Parser *parser);
+void list_statement(Parser *parser);
+void run_statement(Parser *parser);
+void end_statement(Parser *parser);
 
 // Helper functions
 Token *advance_parser(Parser *parser);
+Token *peek_parser(Parser *parser);
 bool at_end_parser(Parser *parser);
 
 #endif // INCLUDE_SRC_PARSER_H_
